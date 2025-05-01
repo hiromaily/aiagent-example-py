30e3c74230c6f1c7e3cd46856edd98f3975253b4
HirokiYasui <hiromaily2@gmail.com>
2025-05-01 14:58:34 +0900

    implement various prompting patterns

--- openai-agents-cli/src/use_cases/prompting.py
+++ openai-agents-cli/src/use_cases/prompting.py
@@ -31,6 +31,12 @@ class PromptingPatternAgent:
             self.few_shot()
         elif pattern == "roll":
             self.roll_prompting()
+        elif pattern == "emotion":
+            self.emotion_prompting()
+        elif pattern == "cot":
+            self.chain_of_thought()
+        elif pattern == "tot":
+            self.tree_of_thoughts()
         else:
             msg = f"Unknown pattern: {pattern}"
             raise ValueError(msg)
@@ -64,12 +70,12 @@ class PromptingPatternAgent:
 
         Q: "I finally achieve my goal!"
         """
-        # execute without instructions
+        # 1. execute without instructions
         logger.info(f"query question: instructions: None, question: {question}")
         response = self._query("", question)
         print(response)
 
-        # execute with instructions
+        # 2. execute with instructions
         instructions = "You are a helpful assistant."
         logger.info(f"query question: instructions: {instructions}, question: {question}")
         response = self._query(instructions, question)
@@ -89,6 +95,85 @@ class PromptingPatternAgent:
         response = self._query(instructions, question)
         print(response)
 
+    def emotion_prompting(self) -> None:
+        """4. Emotion Prompting."""
+        logger.info("Emotion Prompting")
+        instructions = "あなたは私の良き理解者です。ポジティブに応援しましょう。"
+        question = """
+        AIによって社会が大きく変わろうとしています。この状況でもソフトウェアエンジニアとして働き続けることができるのでしょうか？。
+        """
+        logger.info(f"query question: instructions: {instructions}, question: {question}")
+        response = self._query(instructions, question)
+        print(response)
+
+    def chain_of_thought(self) -> None:
+        """5. Chain Of Thought Prompting. + Self-Consistency."""
+        logger.info("Chain Of Thought Prompting")
+        instructions = "You are a helpful assistant."
+        # 1. execute normal question
+        question = """
+        Please count the number of characters in the word `Hallucinations`.
+        """
+        logger.info(f"query question: instructions: {instructions}, question: {question}")
+        response = self._query(instructions, question)
+        print(response)
+
+        # 2. execute with step-by-step
+        question = """
+        Please count the number of characters in the word `Hallucinations`. Think it step-by-step.
+        """
+        logger.info(f"query question: instructions: {instructions}, question: {question}")
+        response = self._query(instructions, question)
+        print(response)
+
+        # 3. Self-Consistency
+        logger.info(f"ask again for self-consistency")
+        response = self._query(instructions, question)
+        print(response)
+
+    def tree_of_thoughts(self) -> None:
+        """6. Tree of Thoughts Prompting. + Self-Consistency."""
+        logger.info("Tree of Thoughts Prompting")
+        instructions = "You are a helpful business consultant."
+        # 1. execute main topic
+        question = """
+        新しいエコフレンドリーなカフェを立ち上げるためのアイデアをいくつか提案してください。
+        """
+        logger.info(f"query question: instructions: {instructions}, question: {question}")
+        response = self._query(instructions, question)
+        print(response)
+
+        # 2. execute idea creation
+        question = """
+        頂いたアイデアのうちの１つを掘り下げてみましょう。3つの異なるアプローチとアイデアを挙げてください。
+        """
+        logger.info(f"query question: instructions: {instructions}, question: {question}")
+        response = self._query(instructions, question)
+        print(response)
+
+        # 3. execute idea evaluation
+        question = """
+        それぞれのアイデアについて、実現可能性・効果・独自性の観点から評価してください。
+        """
+        logger.info(f"query question: instructions: {instructions}, question: {question}")
+        response = self._query(instructions, question)
+        print(response)
+
+        # 4. execute research deeply
+        question = """
+        最も有望なアイデアについて、具体的な実施計画や必要なリソース、リスクとその対策を詳しく説明してください。
+        """
+        logger.info(f"query question: instructions: {instructions}, question: {question}")
+        response = self._query(instructions, question)
+        print(response)
+
+        # 5. execute decision making
+        question = """
+        すべての評価を踏まえ、最も適切と思われるアイデアを1つ選び、その理由を述べてください。
+        """
+        logger.info(f"query question: instructions: {instructions}, question: {question}")
+        response = self._query(instructions, question)
+        print(response)
 
     def _query(self, instructions: str, prompt: str) -> str:
         if self._api_mode == APIMode.RESPONSE_API:
